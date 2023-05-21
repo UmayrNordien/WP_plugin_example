e52cd5d152048d2d35f5dc8fd0bb36e3355e4fee
Umayr Nordien <114750267+UmayrNordien@users.noreply.github.com>
2023-05-21 22:00:51 +0200

    Initial commit

---
+++ code.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Plugin Name: My plugin example
+ */
+
+ /*
+ * Plugin Name:       My Plugin
+ * Author:            Umayr Nordien
+ */
+
+// Remove the admin bar from the front end
+add_filter( 'show_admin_bar', '__return_false' );
